27881dc287a7b308c4a4c1f075c3a7cebd3eb42d
anosov34 <bookmake339@gmail.com>
2021-11-20 21:39:21 +0300

    Initial commit

---
+++ src/main/java/pro/sky/java/course2/calculator/finalcalculator/FinalCalculatorApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.calculator.finalcalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalCalculatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinalCalculatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/java/course2/calculator/finalcalculator/FinalCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.calculator.finalcalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalCalculatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
